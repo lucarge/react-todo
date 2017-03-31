bb6448ba31098802c66957f17e3bb006b2d48b31
Luca Argenziano <luca.argenziano@cubica.eu>
2017-03-31 15:29:03 +0200

    Style todo application

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -6,11 +6,17 @@ var TodoList = React.createClass({
   render: function() {
     var {todos} = this.props;
     var renderTodos = () => {
+      if(todos.length === 0) {
+        return (
+          <p className="container__message">Nothing to do</p>
+        );
+      } else {
       return todos.map((todo) => {
         return (
           <Todo key={todo.id} {...todo} onToggle={this.props.onToggle} />
         );
       });
+      }
     };
 
     return (
