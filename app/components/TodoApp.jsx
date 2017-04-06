3474e143196f17ae57c3462ddff77dc958a8127f
Luca Argenziano <luca.argenziano@cubica.eu>
2017-04-06 18:09:43 +0200

    Refactor TodoList, Todo and AddTodo to work with redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,8 +4,8 @@ var moment = require('moment');
 
 var TodoAPI = require('TodoAPI');
 
-var TodoList = require('TodoList');
-var AddTodo = require('AddTodo');
+import TodoList from 'TodoList';
+import AddTodo from 'AddTodo';
 var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
@@ -33,17 +33,6 @@ var TodoApp = React.createClass({
       ]
     });
   },
-  handleToggle: function(id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined;
-      }
-      return todo;
-    });
-
-    this.setState({todos: updatedTodos});
-  },
   handleSearch: function(showCompleted, searchText) {
     this.setState({
       showCompleted: showCompleted,
@@ -60,7 +49,7 @@ var TodoApp = React.createClass({
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
               <TodoSearch onSearch={this.handleSearch} />
-              <TodoList todos={filteredTodos} onToggle={this.handleToggle} />
+              <TodoList />
               <AddTodo onAddTodo={this.handleAddTodo} />
             </div>
           </div>
