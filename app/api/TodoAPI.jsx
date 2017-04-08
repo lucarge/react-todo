afe5a585d2a3ed8a794e243d86d7c00846fe4e43
Luca Argenziano <luca.argenziano@cubica.eu>
2017-04-08 11:45:07 +0200

    Fetch todos from Firebase on app start

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,24 +1,6 @@
 var $ = require('jquery');
 
 module.exports = {
-  setTodos: function(todos) {
-    if($.isArray(todos)) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-      return todos;
-    }
-  },
-  getTodos: function() {
-    var stringTodos = localStorage.getItem('todos');
-    var todos = new Array;
-
-    try {
-      todos = JSON.parse(stringTodos);
-    } catch (e) {
-
-    }
-
-    return $.isArray(todos) ? todos : new Array;
-  },
   filterTodos: function(todos, showCompleted, searchText) {
     var filteredTodos = todos;
 
--- app/app.jsx
+++ app/app.jsx
@@ -9,15 +9,7 @@ var TodoAPI = require('TodoAPI');
 var actions = require('actions');
 var store = require('configureStore').configure();
 
-store.subscribe(() => {
-  var state = store.getState();
-  console.log('New state', state);
-
-  TodoAPI.setTodos(state.todos);
-});
-
-var initialTodos = TodoAPI.getTodos();
-store.dispatch(actions.addTodos(initialTodos));
+store.dispatch(actions.startAddTodos());
 
 // Load foundation
 $(document).foundation();
