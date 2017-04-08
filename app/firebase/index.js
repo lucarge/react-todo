7067360160883b5c268759f9585327ddb5d8dacc
Luca Argenziano <luca.argenziano@cubica.eu>
2017-04-08 12:28:34 +0200

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyCxLY-sPnwvwTRhpUKgL_L-vpCMb4SlCJg",
-    authDomain: "lucarge-todo-app.firebaseapp.com",
-    databaseURL: "https://lucarge-todo-app.firebaseio.com",
-    projectId: "lucarge-todo-app",
-    storageBucket: "lucarge-todo-app.appspot.com",
-    messagingSenderId: "380439166692"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch(e) {
